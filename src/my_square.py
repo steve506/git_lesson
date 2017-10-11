e020a9cfe58182424932980257e640a3714ba0bb
steve506 <32716681+steve506@users.noreply.github.com>
2017-10-11 13:50:24 -0600

    x -> z and square 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,9 @@
-def my_square(x):
+def my_square(z):
 	'''takes a value and returns the squared value.
 	
-	uses the ** operator
+	uses the * operator
 	
 	'''
-	return (x** 2)
+	return (z* 2)
 	
-print my_square(4)
+print my_square(24)
