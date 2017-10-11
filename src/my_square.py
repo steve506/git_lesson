49d1fbc14fad842750927bd009a9e6ab4e0c784d
Steven Garcia <steven_garcia@mckinsey.com>
2017-10-11 13:49:02 -0600

    x -> y

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,9 @@
-def my_square(x):
+def my_square(y):
 	'''takes a value and returns the squared value.
 	
 	uses the ** operator
 	
 	'''
-	return (x** 2)
+	return (y** 2)
 	
-print my_square(4)
+print my_square(42)
